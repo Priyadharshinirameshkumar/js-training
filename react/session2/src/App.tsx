9262e6a7e4f54d5a97f6e0c7d8f85cb61d64a92b
Priyadharshini R <priyadharshinirameshkumar19@gmail.com>
2026-07-10 17:45:48 +0530

    Complete React Session 2 - Section 3

--- react/session2/src/App.tsx
+++ react/session2/src/App.tsx
@@ -75,25 +75,96 @@
 
 //task 2.2
 
-import ProfileCard from "./ProfileCard"
+// import ProfileCard from "./ProfileCard"
+
+// function App() {
+//   return (
+//     <div>
+
+//       <ProfileCard
+//         name="Rahul"
+//         role="Frontend"
+//         score={92}
+//         skills={["React", "TypeScript", "CSS"]}
+//       />
+
+//       <ProfileCard
+//         name="Priya"
+//         skills={["Node.js", "Express"]}
+//       />
+
+//       <ProfileCard />
+
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+//task 3.1
+
+// import InternProfile from "./InternProfile"
+// import type { Intern } from "./InternProfile"
+
+// function App() {
+
+//   const rahul: Intern = {
+//     id: 1,
+//     name: "Rahul",
+//     score: 92,
+//     isPresent: true,
+//     skills: ["HTML", "CSS", "TypeScript", "React"],
+//   }
+
+//   return (
+//     <div>
+//       <InternProfile intern={rahul} />
+//     </div>
+//   )
+// }
+
+// export default App
+
+//task 3.2
+
+import InternProfile from "./InternProfile"
+import type { Intern } from "./InternProfile"
 
 function App() {
+
+  const rahul: Intern = {
+    id: 1,
+    name: "Rahul",
+    score: 92,
+    isPresent: true,
+    skills: ["HTML", "CSS", "TypeScript", "React"],
+  }
+
+  const priya: Intern = {
+    id: 2,
+    name: "Priya",
+    score: 78,
+    isPresent: true,
+    skills: ["Node.js", "TypeScript"],
+  }
+
   return (
     <div>
 
-      <ProfileCard
-        name="Rahul"
-        role="Frontend"
-        score={92}
-        skills={["React", "TypeScript", "CSS"]}
-      />
+      {/* Passing the original object */}
+      <InternProfile intern={rahul} />
 
-      <ProfileCard
-        name="Priya"
-        skills={["Node.js", "Express"]}
-      />
+      {/* These two are equivalent */}
+      <InternProfile intern={priya} />
 
-      <ProfileCard />
+      <InternProfile intern={{ ...priya }} />
+
+      {/* The spread operator (...) creates a shallow copy of an object.
+          It is useful when creating a new object or updating values without
+          changing the original object. However, using it unnecessarily can
+          make the code harder to read when the original object can be passed
+          directly. */}
 
     </div>
   )
