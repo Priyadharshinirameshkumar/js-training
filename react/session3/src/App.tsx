3b1ffe6e358a393c64a792c68fc2d9447bfe14cf
Priyadharshini R <priyadharshinirameshkumar19@gmail.com>
2026-07-13 17:30:27 +0530

    react/session3: complete section 4

--- react/session3/src/App.tsx
+++ react/session3/src/App.tsx
@@ -7,32 +7,25 @@
 // export default App
 
 //task 1.2
-
 // import StateTypes from "./StateTypes"
-
 // function App() {
 //   return <StateTypes />
 // }
-
 // export default App
 
+
 //task 2.1
-
 // import InternForm from "./InternForm"
-
 // function App() {
 //   return <InternForm />
 // }
-
 // export default App
 
 //task 3.1
 // import TogglePanel from "./TogglePanel"
-
 // function App() {
 //   return <TogglePanel />
 // }
-
 // export default App
 
 
@@ -47,10 +40,30 @@
 
 //task 3.3
 
-import InternList from "./InternList"
+// import InternList from "./InternList"
+// function App() {
+//   return <InternList />
+// }
+// export default App
 
+
+//task 4.1
+// import InternLoader from "./InternLoader"
+// function App() {
+//   return <InternLoader />
+// }
+// export default App
+
+//task 4.2
+// import FilteredInterns from "./FilteredInterns"
+// function App() {
+//   return <FilteredInterns />
+// }
+// export default App
+
+//task 4.3
+import EscapeHandler from "./EscapeHandler"
 function App() {
-  return <InternList />
+  return <EscapeHandler />
 }
-
 export default App
